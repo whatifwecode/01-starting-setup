a28ab905e701ce7145883377f3ec6fd160488873
jegor.kuznetsov <jegor.kuznetsov@bigwintek.com>
2022-09-29 15:59:56 +0300

    saving new values in object

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import './ExpenseForm.css'
 import {useState} from "react";
 
-function ExpenseForm () {
+function ExpenseForm (props) {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState ('');
@@ -47,7 +47,7 @@ function ExpenseForm () {
             date: new Date (enteredDate)
         };
 
-        console.log(expenseData)
+        props.onSaveExpenseData(expenseData)
         setEnteredTitle('')
         setEnteredAmount('')
         setEnteredDate('')
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,18 @@
 import "./NewExpense.css"
 import ExpenseForm from "./ExpenseForm";
+
 function NewExpense () {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+        console.log(expenseData)
+    }
+
         return (
             <div className='new-expense'>
-                <ExpenseForm />
+                <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
             </div>
         );
 }
